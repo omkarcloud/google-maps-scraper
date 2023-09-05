859ead5ab375ed13a12d573e8d0967dea007c832
babidi34 <karimbaidi@gmail.com>
2023-09-05 22:47:43 +0200

    Docker : OK

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM ultrafunk/undetected-chromedriver:106
+FROM python:3.9-bullseye
 
 ENV PYTHONUNBUFFERED=1
 
 COPY requirements.txt .
+RUN apt update && apt install -y chromium
 RUN python -m pip install  -r requirements.txt
 
 RUN mkdir app
--- src/config.py
+++ src/config.py
@@ -3,8 +3,9 @@ Love It? Star It! ⭐ https://github.com/omkarcloud/google-maps-scraper/
 '''
 queries = [
     {
-        "keyword": "restaurants in delhi",
+        "keyword": "agence immobilliere à strasbourg",
         "max_results" : 5,
+        "select": ["title", "link", "main_category", "website", "phone" , "address"],
     },
 ]
 
