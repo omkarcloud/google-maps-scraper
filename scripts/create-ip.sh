53e6aa6b79cfbdfbe0fb456cc8bc0cddf67d11be
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2024-04-01 17:08:03 +0530

    fixes

---
+++ scripts/create-ip.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python -m pip install bota
+python -m bota create-ip --name gmaps
---
+++ scripts/install-scraper.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo apt install -y python3-pip
+alias python=python3
+python -m pip install bota
+python -m bota install-scraper --repo-url https://github.com/omkarcloud/google-maps-scraper
