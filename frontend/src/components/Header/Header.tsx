fe374781b579515cd2e9bb3f064e8ef4c5a3ebad
Jimmy BOYER <boyer.jimmy@gmail.com>
2024-04-30 21:50:02 +0200

    :tada: Begin project !!
    Start by changing Docker to have 4 bot // Trying remove some frontend elements on python framework

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,5 @@
-import { EuiLink } from '@elastic/eui'
-
 const Header = () => (
   <div>
-    <EuiLink href="https://github.com/omkarcloud/botasaurus" target="_blank">
-      Love It? Star It! ★
-    </EuiLink>
   </div>
 )
 
