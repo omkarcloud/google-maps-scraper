0a0821e51bac1675c42a98cf27bbc7c1ca711cb3
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2024-05-16 22:41:43 +0530

    fixes

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -4,17 +4,9 @@ import {
   EuiImage,
   EuiPageTemplate,
 } from '@elastic/eui'
-import { useRouter } from 'next/router'
 
-const NotFoundPage = () => {
+function NotFoundPage(props) {
   const illustration = '/images/404_rainy_cloud_light.png'
-
-  const router = useRouter()
-
-  const handleClick = e => {
-    e.preventDefault()
-    router.back()
-  }
 
   return (
     <EuiPageTemplate>
@@ -24,22 +16,16 @@ const NotFoundPage = () => {
             <EuiButton
               color="primary"
               fill
-              onClick={handleClick}
+              href='/'
               key="404-go-back">
-              Go back
+              Go Home
             </EuiButton>,
           ]}
-          body={
-            <p>
-              Sorry, we can&apos;t find the page you&apos;re looking for. It
-              might have been removed or renamed, or maybe it never existed.
-            </p>
-          }
+          body={<p>{`Sorry, we can't find the page you're looking for. It might have been removed or renamed, or maybe it never existed.`}</p>}
           icon={<EuiImage alt="" size="fullWidth" src={illustration} />}
           layout="vertical"
           title={<h2>Page not found</h2>}
-          titleSize="m"
-        />
+          titleSize="m" />
       </EuiPageTemplate.EmptyPrompt>
     </EuiPageTemplate>
   )
